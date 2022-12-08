cb3f0eee7eb432cceec025ead69de0ac50392c3d
Olivier7355 <108932082+Olivier7355@users.noreply.github.com>
2022-12-08 23:03:15 +0100

    Add files via upload

---
+++ BoulderDash.py
@@ -0,0 +1,222 @@
+# Activate Virtual environment : source venv/bin/activate
+import pygame
+from pygame.locals import *
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        #load images
+        #dirt_img = pygame.image.load('img/dirt.png')
+        #lemmings_img = pygame.image.load('img/lemmings1.png')
+        
+        sprite_sheet_image = pygame.image.load('images/sprites_sheet.png')
+        static_boulder = sprite_sheet_image.subsurface(0, 0, 32, 32)
+        wall = sprite_sheet_image.subsurface(32, 192, 32, 32)
+        dirt = sprite_sheet_image.subsurface(32, 224, 32, 32)
+        rock = sprite_sheet_image.subsurface(0, 224, 32, 32)
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 0:
+                    img = pygame.transform.scale(dirt, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 1:
+                    img = pygame.transform.scale(wall, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(static_boulder, (32, 32))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    img = pygame.transform.scale(rock, (32, 32))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+            #pygame.draw.rect(screen, (255,255,255), tile[1], 2)
+
+class MyLemmings():
+    def __init__(self, x, y):
+        img = pygame.image.load('imgages/sprites_sheet.png')
+        #self.lemmingsStop = pygame.image.load('img/lemmings-stop.png')
+        #self.lemStop = pygame.transform.scale(self.lemmingsStop, (50, 70))
+                
+        self.image = pygame.transform.scale(img, (50, 70))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+        self.animation_list =[]
+        self.animation_steps = 9
+        self.last_update = pygame.time.get_ticks()
+        self.animation_cooldown = 100
+        self.frame = 0
+        
+
+        #for x in range(self.animation_steps) :
+        #    self.animation_list.append(get_image(sprite_sheet_image, x, 160, 240, 0.3))
+
+    """def walking_boulder_animation(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_update >= self.animation_cooldown:
+            self.frame += 1
+            self.last_update = current_time
+            if self.frame >= len(self.animation_list) :
+                self.frame = 0
+    """    
+
+    def update(self):
+        dx = 0
+        dy = 0
+        
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            dx -= 3
+            #self.walking_boulder_animation()
+            
+        if key[pygame.K_RIGHT]:
+            dx += 3
+            #self.walking_boulder_animation()
+  
+        if key[pygame.K_DOWN]:
+            dy += 3
+        if key[pygame.K_UP]:
+            dy -= 3
+        
+        for tile in world.tile_list:
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                dy = 0
+                dx += 3
+                
+                self.sol = True
+            elif tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+                
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # display Boulder
+        if (key[pygame.K_LEFT]) : 
+            screen.blit(pygame.transform.flip(self.animation_list[self.frame], True, False), self.rect)
+        elif (key[pygame.K_RIGHT]) :
+            screen.blit(self.animation_list[self.frame], self.rect)
+        else :
+            #screen.blit(static_boulder, self.rect)
+            pass
+
+
+
+        #pygame.draw.rect(screen, (255,255,255), self.rect, 2)
+
+
+pygame.init()
+clock = pygame.time.Clock()
+fps = 30
+
+screen_width = 1000
+screen_height = 1000
+tile_size = 32
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Boulder Dash')
+
+
+#boulder = MyLemmings()
+
+"""
+def get_image(sheet, frame, width, height, scale):
+    image = pygame.Surface((width, height)).convert_alpha()
+    if frame < 3 :
+        image.blit(sheet, (0,0), ((frame*width),0, width, height))
+
+    if (frame > 2) and (frame < 6) : 
+        image.blit(sheet, (0,0), (((frame-3)*width), 240, width, height))
+
+    if (frame > 5) : 
+        image.blit(sheet, (0,0), (((frame-6)*width), 480, width, height))
+
+    image = pygame.transform.scale(image, (width*scale, height*scale))
+    image.set_colorkey((0,0,0))
+    return image
+"""
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+#exit_img = pygame.image.load('img/exit.png')
+
+world = World(world_data)
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    #screen.blit(bg_img, (0,0))
+    screen.fill((0,0,0))
+
+
+    world.draw()
+
+    #boulder.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+pygame.quit()
